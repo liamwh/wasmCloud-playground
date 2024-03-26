16dd0716fc4517dd45ba11054f9e676a1f4fa858
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-03-26 18:55:30 +0100

    fix: Replace unwraps with expects

--- hello-rust/src/lib.rs
+++ hello-rust/src/lib.rs
@@ -13,13 +13,15 @@ struct HttpServer;
 impl Guest for HttpServer {
     fn handle(_request: IncomingRequest, response_out: ResponseOutparam) {
         let response = OutgoingResponse::new(Fields::new());
-        response.set_status_code(200).unwrap();
+        response
+            .set_status_code(200)
+            .expect("setting status code to 200 should never fail");
         let response_body = response.body().unwrap();
         response_body
             .write()
-            .unwrap()
+            .expect("the first time we call write should always succeed")
             .blocking_write_and_flush(b"Hello from Rust!\n")
-            .unwrap();
+            .expect("a byte string literal smaller than 4096 bytes should always succeed");
         OutgoingBody::finish(response_body, None).expect("failed to finish response body");
         ResponseOutparam::set(response_out, Ok(response));
     }
